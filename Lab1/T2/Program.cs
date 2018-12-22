87d35914c21cc91be90c641c076d2abbe68c108f
Xana <m2069@student.jamk.fi>
2018-12-22 16:27:46 +0200

    LAB1 Assignment 2 added

---
+++ Lab1/T2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program // Luokka johon ohjelma kuuluu 
+    {
+        static void Main()
+        {
+            Console.WriteLine("Give point limit : ");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 12 || number < 0) {
+                Console.WriteLine("Invalid input. Use numbers within range 0-12");
+                Console.ReadKey();
+            }
+            else if (number <= 11) {
+                Console.WriteLine("School number is {0}", number / 2);
+                Console.ReadKey();
+            }
+            else {
+                Console.WriteLine("School number is 5");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
