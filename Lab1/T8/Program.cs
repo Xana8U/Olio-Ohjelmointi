3541dfef372dc6ce2390e0380a01e83871513178
Xana <m2069@student.jamk.fi>
2018-12-22 17:58:08 +0200

    LAB1 T8 added

---
+++ Lab1/T8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8 {
+    class Program {
+        static void Main() {
+            Console.Write("Give a number : ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Give a number : ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("Give a number : ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            List<int> lista = new List<int> { n1, n2, n3 };
+            Console.WriteLine("The biggest number is {0}", lista.Max());
+            Console.ReadKey();
+        }
+    }
+}
+
