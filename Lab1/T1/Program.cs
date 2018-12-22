9d78796ea334b8b5ef343c0d8fcfe3fbc890ddc9
Xana <m2069@student.jamk.fi>
2018-12-22 16:02:55 +0200

    LAB1 Assignment 1 added

--- Lab1/T1/Program.cs
+++ Lab1/T1/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace T1
 {
-    class Program
+    class Program // Luokka johon ohjelma kuuluu 
     {
-        static void Main(string[] args)
-        {
+        static void Main() {  // Ohjelman pääfunktio
+            Dictionary<int, string> kirjasto = new Dictionary<int, string>(){  // Luo uuden kirjaston integer avaimella ja string arvolla
+                    {1, "one"},  // predefined instances
+                    {2, "two"},
+                    {3, "three"}
+            };
+            Console.WriteLine("Give a number > ");  // TUlostaa merkkijonon
+            int number = int.Parse(Console.ReadLine());  // Tekee muuttujan number, odottaa että käyttäjä syöttää merkkijonon.
+            if (number > 3 || number < 1){
+                Console.WriteLine("Joku muu luku");
+                Console.ReadKey();
+            }
+            else{
+            Console.WriteLine("number is {0}", kirjasto.Values.ElementAt(number - 1));  // Lukee teksti arvon kirjastosta käyttäen käyttäjän syöttämää numeroa avaimena
+            Console.ReadKey();
+            }
         }
     }
 }
