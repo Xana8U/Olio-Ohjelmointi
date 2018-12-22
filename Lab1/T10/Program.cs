2c8d9380e7a1c0b58ce501c358be255d06c578ce
Xana <m2069@student.jamk.fi>
2018-12-22 18:42:39 +0200

    LAB1 T9 and T10 added

---
+++ Lab1/T10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10 {
+    class Program {
+        static void Main() {
+            int[] numerot = {
+                1, 2, 33, 44, 55, 68, 77, 96, 100
+            };
+            for (int i = 0; i < numerot.Count(); i++) {
+                if (numerot[i] % 2 == 0) {
+                    Console.WriteLine("HEP");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/T9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace T9 {
+    class Program {
+        static void Main() {
+            int number = 1;
+            List<int> numbers = new List<int>();
+            while (number != 0) {
+                Console.Write("Anna numero : ");
+                number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+            }
+
+            Console.WriteLine("Lukujen summa on {0}", numbers.Sum());
+            Console.ReadKey();
+
+        }
+    }
+}
