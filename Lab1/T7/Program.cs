45213da8249d6e6d075c927e153736934babc8db
Xana <m2069@student.jamk.fi>
2018-12-22 17:44:06 +0200

    LAB1 T7 added

---
+++ Lab1/T7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7 {
+    class Program {
+        static void Main() {
+            Console.Write("Give a year : ");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0) {
+                if (year % 400 == 0) {
+                    Console.WriteLine("Year is a leap year.");
+                    Console.ReadKey();    
+                }
+                else {
+                    Console.WriteLine("Year is not a leap year");
+                    Console.ReadKey();
+                }
+            }
+            else {
+                Console.WriteLine("Year is not a leap year");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
