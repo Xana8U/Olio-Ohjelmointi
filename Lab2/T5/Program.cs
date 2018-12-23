d225edf863723d0144f875a508bc448a4e693785
Xana <m2069@student.jamk.fi>
2018-12-23 04:03:34 +0200

    LAB2 T5 added

---
+++ Lab2/T5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 7
+        *
+       ***
+      *****
+     *******
+    *********
+        *
+        * 
+ */
+
+
+namespace T5 {
+    class Program {
+        static void Main() {
+            Console.Write("Give a number : ");
+            int korkeus = int.Parse(Console.ReadLine());
+            int offset = (korkeus - 2) * 2 / 2;
+            for (int i = 0; i < offset; i++) {
+                string space = String.Concat(Enumerable.Repeat(" ", offset - i));
+                string starL = String.Concat(Enumerable.Repeat("*", i + 1));
+                string starR = String.Concat(Enumerable.Repeat("*", i));
+
+                Console.Write(space);
+                Console.Write(starL);
+                Console.Write(starR);
+               
+                Console.WriteLine("");
+            }
+            string trunk = String.Concat(Enumerable.Repeat(" ", (korkeus - 2) * 2 / 2));
+            Console.WriteLine("{0}*", trunk);
+            Console.WriteLine("{0}*", trunk);
+
+            Console.ReadKey();
+
+        }
+    }
+}
