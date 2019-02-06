446e409519e1ec73a73eabbc534de792fac31bdc
Xana <m2069@student.jamk.fi>
2019-02-06 21:26:20 +0200

    Solution edit   added T6,7,8

---
+++ Lab2/T6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    /*
+     * Tee ohjelma, joka arpoo satunnaisluvun väliltä 0-100. Käytä C#:n Random -luokkaa. 
+     * Tämän jälkeen ohjelman käyttäjää kehoitetaan arvaaman arvottu luku. Ohjelman tulee antaa vihje arvauksen jälkeen onko arvottu luku pienemäi vai suurempi. 
+     * Tämän jälkeen vihjeitä toistetaan kunnes käyttäjä arvaa oikean luvun. Tulosta lopuksi arvausten määrä näytölle.
+     */ 
+    
+namespace T6 {
+    class Program {
+        static void Main() {
+            int tries = 0;
+            Random rand = new Random();
+            int value = rand.Next(0, 101);
+
+            int randomsgame(int args) {
+                tries += args;
+                
+                Console.WriteLine("Guess #{0}", tries);
+                Console.Write("Guess a number: ");
+                int guess;
+                try {
+                    guess = int.Parse(Console.ReadLine());
+                }
+                catch {
+                    randomsgame(1);
+                }
+                
+                if (value < guess) {
+                    Console.WriteLine("Wrong guess, guess lower...");
+                    randomsgame(1);
+                }
+                else if (value > guess) {
+                    Console.WriteLine("Wrong guess, guess higher...");
+                    randomsgame(1);
+                }
+
+                else if (value == guess) {
+                    Console.WriteLine("Congratulations!!! \nThe number indeed was {0}", value);
+                    System.Environment.Exit(0);
+                }
+                return 0;
+            }
+            randomsgame(0);
+        }
+    }
+}
+
---
+++ Lab2/T7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Tee ohjelma, joka lajittelee kahdessa kokonaislukutaulukossa olevat alkiot suurusjärjestykseen kolmanteen kokonaislukutaulukkoon. 
+ * Tulosta lopuksi lajitellun taulukon sisältö.
+ */
+
+namespace T7 {
+    class Program {
+        static void Main(string[] args) {
+            int[] array1 = new int[5] { 10, 20, 30, 40, 50 };
+            int[] array2 = new int[5] { 5, 15, 25, 35, 45 };
+            int[] array3 = new int[array1.Length + array2.Length];
+
+            array3 = array1.Concat(array2).ToArray();
+            for (int i = 0; i < array1.Length; i++) {
+
+            }
+            Console.Write("Numbers in Array 1: ");
+            for (int i = 0; i < array1.Length; i++) {
+                Console.Write("{0},", array1[i]);
+            }
+            Console.Write("\nNumbers in array 2: ");
+            for (int i = 0; i < array2.Length; i++) {
+                Console.Write("{0},", array2[i]);
+            }
+            Array.Sort(array3);
+            Console.Write("\nNumbers in the combined array: ");
+            for (int i = 0; i < array3.Length; i++) {
+                Console.Write("{0},", array3[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
