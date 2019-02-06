18cc6ccd6193bc66ae0b9968d3d1ae2144024fea
Xana <m2069@student.jamk.fi>
2019-02-06 21:53:26 +0200

    Completed T8

--- Lab2/T8/Program.cs
+++ Lab2/T8/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace T8 {
     class Program {
         static void Main(string[] args) {
+            // Tee ohjelma, joka kysyy käyttäjältä merkkijonon(lause).Sovelluksen tulee ilmoittaa käyttäjälle oliko annettu merkkijono palidromi.
+            Console.Write("Insert a string for palindrome test: ");
+            string syote = Console.ReadLine();
+            
+            if (syote.Reverse().SequenceEqual(syote)) {
+                Console.WriteLine("The string \"{0}\" is a palindrome!", syote);
+            }
+            else {
+                Console.WriteLine("The string is not a palindrome");
+            }
         }
     }
 }
